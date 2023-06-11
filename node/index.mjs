6562dc0b5a29f018358ebc89d90a472bc5d421ac
Brian Keng <bjlkeng@users.noreply.github.com>
2023-06-10 21:48:19 -0400

    Doing text splitting in Python, not in JS.

--- node/index.mjs
+++ node/index.mjs
@@ -1,12 +1,12 @@
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { JSONLinesLoader } from "langchain/document_loaders/fs/json";
-import { OpenAI } from "langchain/llms/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { assert } from "console";
 
-const jsonl_filename = "../crawler/briankeng-2023-06-09.jsonl";
+const jsonl_filename = "../crawler/briankeng-split-2023-06-09.jsonl";
 const index_filename = "index.faiss";
 const chunkSize = 1000;
 const chunkOverlap = 100;
@@ -14,28 +14,24 @@ const modelName = "text-davinci-003";
 const embeddingModelName = 'text-embedding-ada-002';
 const maxTokens = 256;
 
+// Assume docs are already pre-split (since langchainjs doesn't have tiktoken tokenizer yet)
 const loader = new JSONLinesLoader(jsonl_filename, "/content");
-const rawDocs = await loader.load();
+const docs = await loader.load();
 
 // Brute force load meta data because jsonl loader doesn't support extracting multiple fields
 const urlLoader = new JSONLinesLoader(jsonl_filename, "/url");
 const urlDocs = await urlLoader.load();
-
 const titleLoader = new JSONLinesLoader(jsonl_filename, "/title");
 const titleDocs = await titleLoader.load();
 
-assert(rawDocs.length == urlDocs.length, "rawDocs and urlDocs must be the same length");
-assert(rawDocs.length == titleDocs.length, "rawDocs and titleDocs must be the same length");
+assert(docs.length == urlDocs.length, "docs and urlDocs must be the same length");
+assert(docs.length == titleDocs.length, "docs and titleDocs must be the same length");
 
-// add url metadata to rawDocs
-for (let i = 0; i < rawDocs.length; i++) {
-    rawDocs[i].metadata["url"] = urlDocs[i].pageContent;
-    rawDocs[i].metadata["title"] = titleDocs[i].pageContent;
+// add url metadata to docs
+for (let i = 0; i < docs.length; i++) {
+    docs[i].metadata["url"] = urlDocs[i].pageContent;
+    docs[i].metadata["title"] = titleDocs[i].pageContent;
 }
-
-// const text = fs.readFileSync("../test.txt", "utf8");
-const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: chunkSize, chunkOverlap: chunkOverlap });
-const docs = await textSplitter.splitDocuments(rawDocs);
 
 console.log(docs)
 
