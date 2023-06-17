e3f7df2c5a956332f276ed2561e2b88acef03acc
Brian Keng <bjlkeng@users.noreply.github.com>
2023-06-16 22:03:23 -0400

    Tryign to switch to custom doman + use unbound -- still waiting.

--- README.md
+++ README.md
@@ -5,8 +5,14 @@ Note: I know the code is a mess but this is a one-off project to play with LLM's
 
 # Notes
 
+## 2023-06-14
+Got it working end to end!  It reads it from R2, indexes it, and then runs the query usign langchainjs.  
+On the `*.workers.dev` domain Cloudflare seems to not enforce the CPU limits, but when I route it through
+my `bjlkeng.io` domain, it looks like it enforces the 10ms CPU time.  Looks like I'll have to jump on the paid
+plan and use the unbounded worker type.
+
 ## 2023-06-13
-* Instead of serializing index, just re-build index from scratch using the
-  crawled, split JSON because none of the indexes in langchainJS can read from
-  arbitrary strings.  All the non-server index save/load functionality require a
-  filesystem (and they seem to be written in C/C++), so it's too complicated to do it otherwise.  It'll be a bit more CPU time to re-index every call, but the actual filesize is roughly the same anyways, let's see if Workers can handle that.
+Instead of serializing index, just re-build index from scratch using the
+crawled, split JSON because none of the indexes in langchainJS can read from
+arbitrary strings.  All the non-server index save/load functionality require a
+filesystem (and they seem to be written in C/C++), so it's too complicated to do it otherwise.  It'll be a bit more CPU time to re-index every call, but the actual filesize is roughly the same anyways, let's see if Workers can handle that.
--- bjlkengbot/index.js
+++ bjlkengbot/index.js
@@ -30,13 +30,17 @@ export default {
   async fetch(request, env, context) {
     const { searchParams } = new URL(request.url);
     const query  = searchParams.get('query');
+    console.log("time1: " + new Date().getTime())
     if (query == null) {
       return new Response('Missing "query" parameter', { status: 400 });
+    } else {
+      return new Response('query: ' + query, { status: 200 });
     }
 
     // Re-index all documents every call because there's not easy way to load an
     // index from the filesystem using Workers (and I didn't want to setup a
     // separate database)
+    console.log("time2: " + new Date().getTime())
     const r2obj = await env.r2bucket.get(env.DOC_PATH);
     const docs = await r2obj.json();
     const embeddingModel = new OpenAIEmbeddings({ 
@@ -44,12 +48,14 @@ export default {
       chunkSize: chunkSize,
       openAIApiKey: env.OPENAI_API_KEY,
     });
+    console.log("time3: " + new Date().getTime())
     const vectorStore = await MemoryVectorStore.fromTexts(
       docs.map(d => d.content),
       docs.map(d => d.metadata),
       embeddingModel,
     );
 
+    console.log("time4: " + new Date().getTime())
     const retriever = vectorStore.asRetriever(numDocsRetrieved);
     const model = new OpenAI({ 
       model: modelName, 
@@ -57,19 +63,23 @@ export default {
       maxTokens: maxTokens, 
       openAIApiKey: env.OPENAI_API_KEY,
     });
+    console.log("time5: " + new Date().getTime())
     const chain = new RetrievalQAChain( {
         combineDocumentsChain: loadQAStuffChain(model, {prompt: prompt }),
         retriever: retriever,
         returnSourceDocuments: true,
     })
 
+    console.log("time6: " + new Date().getTime())
     const res = await chain.call({
       query: query,
     });
 
     res['query'] = query;
 
+    console.log("time7: " + new Date().getTime())
     const json = JSON.stringify(res, null, 2);
+    console.log("time8: " + new Date().getTime())
     return new Response(json, {
       headers: {
         "content-type": "application/json;charset=UTF-8",
