a5161bfe887bbb29f1326fb07cb357b8983966dd
Brian Keng <bjlkeng@users.noreply.github.com>
2023-07-19 20:47:59 -0400

    Adding scripts to measure ROUGE score for both fine tuned and QA prompt only.

--- finetune/fine_tune.sh
+++ finetune/fine_tune.sh
@@ -1,11 +1,20 @@
 
-openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 4 --learning_rate_multiplier 0.1  --batch_size 1 
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 4 --learning_rate_multiplier 0.1  --batch_size 1 
 # [2023-07-10 20:42:38] Created fine-tune: ft-ZXfUBUMbeJhfol9iJkDMDO0e
 
 
-openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 2 --learning_rate_multiplier 0.1  --batch_size 1
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 2 --learning_rate_multiplier 0.1  --batch_size 1
 # [2023-07-10 20:42:47] Created fine-tune: ft-DxMUVJXD4RokyEXMdB7dSdFy
 
 
-openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 1 --learning_rate_multiplier 0.1  --batch_size 1
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl -m curie --suffix "bjlkengbot" --n_epochs 1 --learning_rate_multiplier 0.1  --batch_size 1
 # [2023-07-10 20:42:54] Created fine-tune: ft-BSb89HPmjHCGfzHd7KOElyuD
+
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 2 --learning_rate_multiplier 0.1  --batch_size 5
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 4 --learning_rate_multiplier 0.1  --batch_size 5
+#
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 2 --learning_rate_multiplier 0.05  --batch_size 1
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 2 --learning_rate_multiplier 0.2  --batch_size 1
+#
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 4 --learning_rate_multiplier 0.05  --batch_size 1
+#openai api fine_tunes.create -t briankeng-2023-06-09-finetune.jsonl --no_check_if_files_exist --no_follow -m curie --suffix "bjlkengbot" --n_epochs 4 --learning_rate_multiplier 0.2  --batch_size 1
